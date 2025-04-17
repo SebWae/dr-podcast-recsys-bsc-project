91c48994d4e946f41dce2fa64172ceee57b51c1b
sewr <sewr@dr.dk>
2025-04-17 21:47:43 +0200

    variables for metadata extraction

--- config.py
+++ config.py
@@ -4,6 +4,7 @@ FILTERED_DATA_PATH = "data\podcast_data_filtered.parquet"
 MIN_CONTENT_TIME_SPENT = 60  
 RAW_DATA_PATH = "data\podcast_data_raw.parquet"
 START_DATE = "2024-09-02 00:00:00"
+TRANSFORMED_DATA_PATH = "data\podcast_data_transformed.parquet"
 VAR_RENAME_DICT = {"Date:Time (evar16)":           "date_time",
                    "UserID Visit (evar95)":        "user_id",
                    "Production Number (evar35)":   "prd_number",
@@ -20,10 +21,23 @@ VAR_RENAME_DICT = {"Date:Time (evar16)":           "date_time",
                    "Kategori":                     "category",
                    "Content Time Spent (revent1)": "content_time_spent",
                    }
-TRANSFORMED_DATA_PATH = "data\podcast_data_transformed.parquet"
 
 
 # for extracting metadata
+DESCRIPTION_VAR_RENAME_DICT = {"PRD_NUMBER":       "prd_number",
+                               "PRD_SHORTDESCR":   "episode_description",
+                               }
+EPISODE_DESCRIPTION_PATH = "data\episode_descriptions.parquet"
+METADATA_COLUMNS = {"series_title":         "first",
+                    "unique_title":         "first",
+                    "pub_date":             "first",
+                    "episode_duration":     "first",
+                    "genre":                "first",
+                    "branding_channel":     "first",
+                    "mother_channel":       "first",
+                    "category":             "first",
+                    "episode_description":  "first",
+                    }
 
 
 # for train-test splitting
