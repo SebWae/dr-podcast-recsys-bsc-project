d1575fa6a2d1f67594de5893c5f95f38f2d19826
sewr <sewr@dr.dk>
2025-04-17 18:01:08 +0200

    script for extracting metadata on the episodes

---
+++ scripts/3_extract_metadata.py
@@ -0,0 +1,3 @@
+# TODO: split "unik titel" column to obtain episode title
+# TODO: remove stopwords from episode descriptions and apply stemming
+# TODO: create embeddings for various level of metadata and weighting schemes
