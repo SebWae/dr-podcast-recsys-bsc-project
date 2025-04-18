b184f67af6f5f6d79b4834e4d95020f55091544a
sewr <sewr@dr.dk>
2025-04-18 15:18:20 +0200

    correcting split date

--- config.py
+++ config.py
@@ -44,7 +44,7 @@ METADATA_COLUMNS = {"series_title":         "first",
 COLUMNS_TO_KEEP = ["user_id", "prd_number", "completion_rate"]
 MIN_PLAYS_PER_EPISODE = 10
 MIN_PLAYS_PER_USER = 2
-SPLIT_DATE = "2024-11-01"
+SPLIT_DATE = "2024-11-11"
 TEST_DATA_PATH = "data\podcast_data_test.parquet"
 TRAIN_DATA_PATH = "data\podcast_data_train.parquet"
 
