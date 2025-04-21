e4a063111d3f3587faa066fff68764598830893d
sewr <sewr@dr.dk>
2025-04-21 18:24:18 +0200

    multiple cb recommenders in one script

--- scripts/05c_cb_recommender.py
+++ scripts/05c_cb_recommender.py
@@ -19,8 +19,13 @@ from config import (
     N_RECOMMENDATIONS,
     N_EPOCHS,
     EPSILON,
-    RECOMMENDATIONS_KEY_CB,
     RECOMMENDATIONS_PATH,
+    EMBEDDINGS_TITLE_PATH,
+    EMBEDDINGS_DESCR_PATH,
+    EMBEDDINGS_COMBI_PATH,
+    RECOMMENDATIONS_KEY_CB_COMBI,
+    RECOMMENDATIONS_KEY_CB_DESCR,
+    RECOMMENDATIONS_KEY_CB_TITLE
 )
 import utils
 
@@ -43,47 +48,61 @@ item_list = sorted(train_df['prd_number'].unique().tolist())
 user_mapping = {user: i for i, user in enumerate(user_list)}
 item_mapping = {i: item for i, item in enumerate(item_list)}
 
-item_matrix = item_features.drop(columns='prd_number').values
-item_matrix_csr = csr_matrix(item_matrix)
-
 # LightFM model
 cb_model = LightFM(loss="logistic", 
                    no_components=N_COMPONENTS, 
                    random_state=RANDOM_STATE)
 
-# initializing recommendations
-prev_recommendations = ["0" for _ in range(n_users * N_RECOMMENDATIONS)]
+# paths to embedding locations
+emb_paths = [EMBEDDINGS_TITLE_PATH, EMBEDDINGS_DESCR_PATH, EMBEDDINGS_COMBI_PATH]
+cb_keys = [RECOMMENDATIONS_KEY_CB_TITLE, RECOMMENDATIONS_KEY_CB_DESCR, RECOMMENDATIONS_KEY_CB_COMBI]
 
+for path, key in zip(emb_paths, cb_keys):
+    print(f"\n Generating recommendations for {key}:")
+    # initializing recommendations
+    prev_recommendations = ["0" for _ in range(n_users * N_RECOMMENDATIONS)]
 
-for epoch in tqdm(range(N_EPOCHS)):
-    print("\n Epoch", epoch + 1)
+    # loading embeddings
+    emb_df = pd.read_parquet(path)
 
-    # fitting the model
-    cb_model.fit_partial(interactions=interaction_matrix, item_features=item_matrix_csr)
+    # formatting the emb_df 
+    emb_df = emb_df.rename(columns={"episode": "prd_number"})
+    emb_dict = emb_df.to_dict(orient="list")
+    emb_df_formatted = pd.DataFrame(emb_dict)
 
-    # getting the top N recommendations for all users
-    recommendations = utils.get_top_n_recommendations_all_users(model=cb_model, 
-                                                                interaction_matrix=interaction_matrix, 
-                                                                user_list=user_list, 
-                                                                item_mapping=item_mapping, 
-                                                                n=N_RECOMMENDATIONS,
-                                                                item_matrix=item_matrix_csr)
-    
-    # computing the proportion of changed recommendations
-    diff_percentage = utils.compare_lists(prev_recommendations, recommendations)
-    print(f"{diff_percentage*100:.2f}% of the recommendations changed.", )
+    # turning the embedding dataframe into an csr matrix
+    item_matrix = emb_df_formatted.drop(columns='prd_number').values
+    item_matrix_csr = csr_matrix(item_matrix)
 
-    # stopping if less than <EPSILON> of the recommendations are changing
-    if diff_percentage < EPSILON:
-        print("Stopping early")
-        print("Extracting recommendations")
-        recs_dict = utils.extract_recommendations(recommendations=recommendations,
-                                                  user_mapping=user_mapping,
-                                                  n_recs=N_RECOMMENDATIONS,
-                                                  recommendations_key=RECOMMENDATIONS_KEY_CB)
-        print("Saving recommendations")
-        utils.save_dict_to_json(data_dict=recs_dict,
-                                file_path=RECOMMENDATIONS_PATH)
-        break
-    
-    prev_recommendations = recommendations
+    for epoch in tqdm(range(N_EPOCHS)):
+        print("\n Epoch", epoch + 1)
+
+        # fitting the model
+        cb_model.fit_partial(interactions=interaction_matrix, item_features=item_matrix_csr)
+
+        # getting the top N recommendations for all users
+        recommendations = utils.get_top_n_recommendations_all_users(model=cb_model, 
+                                                                    interaction_matrix=interaction_matrix, 
+                                                                    user_list=user_list, 
+                                                                    item_mapping=item_mapping, 
+                                                                    n=N_RECOMMENDATIONS,
+                                                                    item_matrix=item_matrix_csr)
+        
+        # computing the proportion of changed recommendations
+        diff_percentage = utils.compare_lists(prev_recommendations, recommendations)
+        print(f"{diff_percentage*100:.2f}% of the recommendations changed.", )
+
+        # stopping if less than <EPSILON> of the recommendations are changing
+        if diff_percentage < EPSILON:
+            print("Stopping early")
+            print("Extracting recommendations")
+            recs_dict = utils.extract_recommendations(recommendations=recommendations,
+                                                    user_mapping=user_mapping,
+                                                    n_recs=N_RECOMMENDATIONS,
+                                                    recommendations_key=key)
+            print("Saving recommendations")
+            utils.save_dict_to_json(data_dict=recs_dict,
+                                    file_path=RECOMMENDATIONS_PATH)
+            break
+        
+        prev_recommendations = recommendations
