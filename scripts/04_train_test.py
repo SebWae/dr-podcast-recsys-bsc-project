a31b902eea2e2a048dab99a4f053b9e305fe5f01
swae <swae@itu.dk>
2025-04-23 22:09:42 +0200

    moved filtering on time periods to filter script

--- scripts/04_train_test.py
+++ scripts/04_train_test.py
@@ -7,10 +7,11 @@ sys.path.append(os.path.abspath(os.path.join(os.getcwd(), ".")))
 
 from config import (
     TRANSFORMED_DATA_PATH,
-    SPLIT_DATE,
-    MIN_PLAYS_PER_USER,
+    SPLIT_DATE_TRAIN_VAL,
+    SPLIT_DATE_VAL_TEST,
     COLUMNS_TO_KEEP,
     TRAIN_DATA_PATH,
+    VAL_DATA_PATH,
     TEST_DATA_PATH,
 )
 
@@ -19,27 +20,16 @@ from config import (
 transformed_df = pd.read_parquet(TRANSFORMED_DATA_PATH)
 
 # applying the global user split
-int_train_df = transformed_df[transformed_df['date'] < SPLIT_DATE]
-int_test_df = transformed_df[transformed_df['date'] >= SPLIT_DATE]
-
-# number of unique users both in the intermediary train and test data
-common_users = set(int_train_df['user_id']).intersection(set(int_test_df['user_id']))
-
-# filter df according to the common users
-train_df_common = int_train_df[int_train_df['user_id'].isin(common_users)]
-test_df_common = int_test_df[int_test_df['user_id'].isin(common_users)]
-
-# grouping by user_id and counting the number of prd_numbers for each user in the train data
-df_grouped_train = train_df_common.groupby('user_id')['prd_number'].count()
-
-# filtering away users below threshold for number of plays per user
-train_df = train_df_common[train_df_common['user_id'].isin(df_grouped_train[df_grouped_train >= MIN_PLAYS_PER_USER].index)]
-test_df = test_df_common[test_df_common['user_id'].isin(df_grouped_train[df_grouped_train >= MIN_PLAYS_PER_USER].index)]
+int_train_df = transformed_df[transformed_df["date"] < SPLIT_DATE_TRAIN_VAL]
+int_val_df = transformed_df[(transformed_df["date"] >= SPLIT_DATE_TRAIN_VAL) & (transformed_df["date"] < SPLIT_DATE_VAL_TEST)]
+int_test_df = transformed_df[transformed_df["date"] >= SPLIT_DATE_VAL_TEST]
 
 # only keeping relevant columns
-train_df = train_df[COLUMNS_TO_KEEP]
-test_df = test_df[COLUMNS_TO_KEEP]
+train_df = int_train_df[COLUMNS_TO_KEEP]
+val_df = int_val_df[COLUMNS_TO_KEEP]
+test_df = int_test_df[COLUMNS_TO_KEEP]
 
 # saving train and test data to parquet files
 train_df.to_parquet(TRAIN_DATA_PATH)
+val_df.to_parquet(VAL_DATA_PATH)
 test_df.to_parquet(TEST_DATA_PATH)
