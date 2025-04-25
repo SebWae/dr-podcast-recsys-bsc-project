511b21cc4f59a7ae7b99c1103235b7155e1eb89e
sewr <sewr@dr.dk>
2025-04-25 15:24:10 +0200

    loading json test

--- scripts/test.py
+++ scripts/test.py
@@ -1,46 +1,15 @@
-from sentence_transformers import SentenceTransformer
-import nltk
-from nltk.corpus import stopwords
-from nltk.stem import SnowballStemmer
-nltk.download('stopwords')
+import os
+import pandas as pd
+import sys
 
-embedding_dict = {}
+# adding the parent directory to the Python path
+sys.path.append(os.path.abspath(os.path.join(os.getcwd(), ".")))
 
-# sentence transformer model used to obtain embeddings
-model = SentenceTransformer('paraphrase-multilingual-MiniLM-L12-v2')
+import json
 
-# Danish stopwords
-stop_words = set(stopwords.words('danish'))
+# Load the JSON file
+with open("utils/utils.json", "r") as file:
+    data = json.load(file)
+show_episode_dict = data["show_episodes_dict"]
 
-# Danish stemmer
-stemmer = SnowballStemmer("danish")
-
-
-
-texts = [" ", ""]
-
-for i, text in enumerate(texts):
-    # Tokenize the text (split it into words)
-    words = text.split()
-
-    # Remove stopwords
-    filtered_words = [word for word in words if word.lower() not in stop_words]
-
-    # Join the words back into a sentence
-    filtered_text = " ".join(filtered_words)
-
-    print(filtered_text)
-
-    # Stem the filtered words
-    stemmed_words = [stemmer.stem(word) for word in filtered_words]
-
-    # Join the words back into a sentence
-    stemmed_text = " ".join(stemmed_words)
-    print(stemmed_text)
-    embedding = model.encode(stemmed_text).tolist()
-
-    embedding_dict[i] = embedding
-
-print(embedding_dict)
-
-
+print(show_episode_dict["Genvej"])
