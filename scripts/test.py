f64df8a2b0886c31d0d81dae61ee56a75c49a2f4
sewr <sewr@dr.dk>
2025-04-26 18:57:32 +0200

    testing BiasedMF model from lenskit

--- scripts/test.py
+++ scripts/test.py
@@ -1,15 +1,27 @@
-import os
+
+from lenskit.algorithms.als import BiasedMF
 import pandas as pd
-import sys
+import numpy as np
 
-# adding the parent directory to the Python path
-sys.path.append(os.path.abspath(os.path.join(os.getcwd(), ".")))
+# your data should be a DataFrame with columns: user, item, rating
+ratings = pd.DataFrame({
+    'user': ["1", "2", "1", "3", "2"],
+    'item': [10, 10, 20, 30, 30],
+    'rating': [0.9, 0.9, 0.9, 0.5, 0.9]
+})
 
-import json
+# Set up the matrix factorization model
+mf = BiasedMF(features=20, iterations=50, damping=5.0)
 
-# Load the JSON file
-with open("utils/utils.json", "r") as file:
-    data = json.load(file)
-show_episode_dict = data["show_episodes"]
+# Train the model
+mf.fit(ratings)
 
-print(show_episode_dict["Genvej"])
+# Predict scores
+scores = mf.predict_for_user("1", [10, 20, 30])
+
+# normalizing the scores
+norm = np.linalg.norm(scores)
+scores = (np.array(scores) / norm).tolist()
+print(scores)
+# for item, score in scores:
+#     print(item, score)
