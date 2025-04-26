8b6b43a88ec40854623f640e7c5082e7f9ce3f40
sewr <sewr@dr.dk>
2025-04-26 18:56:26 +0200

    script to run optimal BiasedMF model

--- scripts/05b_cf_recommender.py
+++ scripts/05b_cf_recommender.py
@@ -1,8 +1,9 @@
 import csv
+from itertools import product
 import os
 import sys
 
-from lightfm import LightFM
+from lenskit.algorithms.als import BiasedMF
 import numpy as np
 import pandas as pd
 from tqdm import tqdm
@@ -14,10 +15,13 @@ from config import (
     TRAIN_DATA_PATH,
     VAL_DATA_PATH,
     METADATA_PATH,
-    RANDOM_STATE,
     N_EPOCHS,
     EXPERIMENTS_CF_PATH,
     N_RECOMMENDATIONS,
+    N_COMPONENTS,
+    DAMPING,
+    REG,
+    OPTIMAL_CF_PATH,
     RECOMMENDATIONS_KEY_CF,
     RECOMMENDATIONS_PATH,
 )
@@ -34,89 +38,86 @@ meta_df = pd.read_parquet(METADATA_PATH)
 train_w_meta = pd.merge(train_df, meta_df, on="prd_number", how="left")
 
 # grouping by user_id and series_title
-print("Constructing user-show interaction matrix.")
+print("Constructing user-show dataframe.")
 grouped_df = train_w_meta.groupby(["user_id", "series_title"]).agg(
     avg_completion_rate =   ("completion_rate", "mean"),
     n_episodes =            ("prd_number", "count")
     ).reset_index()
 
 # computing ratings 
+print("Computing user-show ratings.")
 grouped_df["cf_rating"] = grouped_df["avg_completion_rate"] * np.log10(grouped_df["n_episodes"] + 10)
 
-# preparing the interaction matrix
-interaction_matrix = utils.prep_interaction_matrix(df=grouped_df, 
-                                                   user_col="user_id", 
-                                                   item_col="series_title", 
-                                                   rating_col="cf_rating")
+# renaming columns
+ratings_df = grouped_df.rename(columns={"user_id": "user",
+                                        "series_title": "item",
+                                        "cf_rating": "rating"})
 
 # list of users and items
 user_list = sorted(train_df['user_id'].unique().tolist())
 item_list = sorted(train_w_meta['series_title'].unique().tolist())
-all_items = meta_df["prd_number"].unique()
 
-# user and item mappings
-user_mapping = {user: i for i, user in enumerate(user_list)}
+# mapping indices to shows
 show_mapping = {i: item for i, item in enumerate(item_list)}
 
 # dictionary containing completion rates for each user and consumed item in the validation data
+print("Generating completion_rates_dict from validation data.")
 completion_rates_dict = utils.get_ratings_dict(data=val_df, 
                                                user_col="user_id", 
                                                item_col="prd_number", 
                                                ratings_col="completion_rate") 
 
 # values for no_components to test
-n_components_values = [10, 20, 30, 40, 50, 60, 70, 80]
+n_components_values = [10, 20, 30, 40, 50]
+damping_values = [1, 5, 10]
+reg_values = [0.01, 0.05, 0.1]
 
 # performing hyperparameter experiments for cf recommender
 print("Performing hyperparameter experiments.")
-print(f"Values for n_components to test: {n_components_values}")
+print(f"Testing values: n_components={n_components_values}, damping={damping_values}, reg={reg_values}")
 
-for n_components in n_components_values:
-    print(f"n_components: {n_components}")
-    # initializing LightFM model
-    cf_model = LightFM(loss="logistic", 
-                       no_components=n_components, 
-                       random_state=RANDOM_STATE)
+for n_components, damping, reg in product(n_components_values, damping_values, reg_values):
+    print(f"\nTesting combination: features={n_components}, damping={damping}, reg={reg}")
     prev_ndcg = 0
+    for epochs in tqdm(range(1, N_EPOCHS+1)):
+        print(f"\n Epoch {epochs}:")
 
-    for epoch in tqdm(range(N_EPOCHS)):
-        print("\n Epoch", epoch + 1)
+        # initializing BiasedMF model
+        mf = BiasedMF(features=n_components, damping=damping, reg=reg, iterations=epochs)
 
         # fitting the model
-        cf_model.fit_partial(interaction_matrix)
+        mf.fit(ratings_df)
 
         # getting scores for each item for each user
-        episode_scores = utils.get_scores_all_items(model=cf_model, 
-                                                    interaction_matrix=interaction_matrix, 
-                                                    user_mapping=user_mapping, 
-                                                    item_mapping=show_mapping,
-                                                    item_list=all_items)
-        
+        episode_scores = utils.get_cf_scores_lenskit(model=mf, 
+                                                     items=item_list,
+                                                     users=user_list,
+                                                     item_mapping=show_mapping)
+
         recs_dict = utils.extract_recs(scores_dict=episode_scores,
-                                       n_recs=10)
+                                       n_recs=N_RECOMMENDATIONS)
 
         # computing ndcg@10
         ndcgs = []
         for user_id, rec_items in recs_dict.items():
             gain_dict = completion_rates_dict[user_id]
-            optimal_items = sorted(gain_dict, key=lambda x: gain_dict[x], reverse=True)
+            optimal_items = sorted(gain_dict, key=lambda x: gain_dict[x], reverse=True)[:N_RECOMMENDATIONS]
             dcg = utils.compute_dcg(rec_items, gain_dict)
             dcg_star = utils.compute_dcg(optimal_items, gain_dict)
             ndcg_user = dcg / dcg_star 
             ndcgs.append(ndcg_user)
 
         ndcg = np.mean(ndcgs)
-        print(f"ndcg@10: {ndcg:.4f}")
+        print(f"ndcg@10: {ndcg:.10f}")
 
         # stopping if ndcg@10 decreases compared to previous epoch 
-        if ndcg < prev_ndcg:
-            print("ndcg@10 has decreased.")
+        if ndcg <= prev_ndcg:
+            print("ndcg@10 has decreased or is unchanged.")
             print("Stopping early.")
             print(f"Saving experiment results to {EXPERIMENTS_CF_PATH}.")
 
             # writing row to csv
-            row = [n_components, ndcg]
-
+            row = [n_components, damping, reg, prev_ndcg]
             with open(EXPERIMENTS_CF_PATH, mode="a", newline="") as file:
                 writer = csv.writer(file)
                 writer.writerow(row)
@@ -125,5 +126,55 @@ for n_components in n_components_values:
 
         prev_ndcg = ndcg
 
-# training the actual cf recommender with the chosen value for n_components and saving the final recommendations
+# generating recommendations from mf model using optimal hyperparameters
+for epochs in tqdm(range(1, N_EPOCHS+1)):
+    print(f"\n Epoch {epochs}:")
 
+    # initializing BiasedMF model
+    mf = BiasedMF(features=N_COMPONENTS, damping=DAMPING, reg=REG, iterations=epochs)
+
+    # fitting the model
+    mf.fit(ratings_df)
+
+    # getting scores for each item for each user
+    episode_scores = utils.get_cf_scores_lenskit(model=mf, 
+                                                    items=item_list,
+                                                    users=user_list,
+                                                    item_mapping=show_mapping)
+
+    recs_dict = utils.extract_recs(scores_dict=episode_scores,
+                                    n_recs=N_RECOMMENDATIONS)
+
+    # computing ndcg@10
+    ndcgs = []
+    for user_id, rec_items in recs_dict.items():
+        gain_dict = completion_rates_dict[user_id]
+        optimal_items = sorted(gain_dict, key=lambda x: gain_dict[x], reverse=True)[:N_RECOMMENDATIONS]
+        dcg = utils.compute_dcg(rec_items, gain_dict)
+        dcg_star = utils.compute_dcg(optimal_items, gain_dict)
+        ndcg_user = dcg / dcg_star 
+        ndcgs.append(ndcg_user)
+
+    ndcg = np.mean(ndcgs)
+    print(f"ndcg@10: {ndcg:.4f}")
+
+    # saving result
+    row = [epochs, ndcg]
+    with open(OPTIMAL_CF_PATH, mode="a", newline="") as file:
+                writer = csv.writer(file)
+                writer.writerow(row)
+
+    # stopping if ndcg@10 decreases compared to previous epoch 
+    if ndcg <= prev_ndcg:
+        print("ndcg@10 has decreased or is unchanged.")
+        print("Stopping early.")
+
+        # saving recommendations
+        print(f"Saving recommendations to {RECOMMENDATIONS_PATH}.")
+        final_recs_dict = {RECOMMENDATIONS_KEY_CF: prev_recs}
+        utils.save_dict_to_json(data_dict=final_recs_dict, 
+                                file_path=RECOMMENDATIONS_PATH)
+        break
+
+    prev_ndcg = ndcg
+    prev_recs = recs_dict
