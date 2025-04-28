89e1420ae001adfec24d2f6082d118f4df2fa019
sewr <sewr@dr.dk>
2025-04-28 10:28:19 +0200

    pipeline to run multiple scripts

---
+++ pipeline.py
@@ -0,0 +1,25 @@
+import subprocess
+
+# scripts to execute in the pipeline
+scripts = [
+    "scripts/01_filter.py",
+    "scripts/02_transform.py",
+    "scripts/03a_extract_metadata.py",
+    "scripts/03b_extract_embeddings.py",
+    "scripts/04_train_test.py",
+    "scripts/05a_baseline_recommender.py",
+    "scripts/05c_cf_recommender.py",
+    "scripts/05d_cb_recommender.py",
+    "scripts/05f_hybrid_recommender.py",
+    "scripts/06_evaluation.py"
+]
+
+# executing each script in order
+for script in scripts:
+    print(f"Running {script}...")
+    result = subprocess.run(["python", script], check=True)
+    if result.returncode == 0:
+        print(f"{script} completed successfully.")
+    else:
+        print(f"Error occurred while running {script}.")
+        break
