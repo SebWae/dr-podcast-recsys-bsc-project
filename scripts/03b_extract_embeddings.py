a204684c0398859c54bf4c34a38c340b84127ca9
swae <swae@itu.dk>
2025-05-06 20:41:04 +0200

    updated print statements

--- scripts/03b_extract_embeddings.py
+++ scripts/03b_extract_embeddings.py
@@ -41,7 +41,7 @@ stemmer = SnowballStemmer("danish")
 meta_df = pd.read_parquet(METADATA_PATH)
 
 # computing embeddings for each episode
-print("Computing embeddings.")
+print("Computing embeddings based on episode titles and descriptions.")
 for _, row in tqdm(meta_df.iterrows(), total=len(meta_df)):
     # extracting prd_number and textual features
     prd_number = row["prd_number"]
@@ -87,7 +87,7 @@ for i, emb_dict in enumerate(embedding_dicts.values()):
     emb_df.to_parquet(path, index=False)
 
 # computing and saving combi embeddings
-print("Generating combi embeddings.")
+print("Computing combi embeddings.")
 items = meta_df["prd_number"].unique()
 combi_embeddings = {}
 
@@ -101,4 +101,4 @@ print("Saving combi embeddings.")
 combi_emb_df = pd.DataFrame(combi_embeddings)
 combi_emb_df.to_parquet(EMBEDDINGS_COMBI_PATH, index=False)
 
-print("Done! Embeddings have been saved to embeddings folder.")
+print("Done! All embeddings have been saved to embeddings folder.")
