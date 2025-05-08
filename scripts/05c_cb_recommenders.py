405d2425dc37c7468ca900ff69fad5146c819f0b
sewr <sewr@dr.dk>
2025-05-08 12:29:05 +0200

    not saving any scores for cb recommender

--- scripts/05c_cb_recommenders.py
+++ scripts/05c_cb_recommenders.py
@@ -23,9 +23,6 @@ from config import (
     RECOMMENDATIONS_KEY_CB_TITLE,
     RECOMMENDATIONS_KEY_CB_DESCR,
     RECOMMENDATIONS_KEY_CB_COMBI,
-    SCORES_PATH_CB_COMBI,
-    SCORES_PATH_CB_DESCR,
-    SCORES_PATH_CB_TITLE,
     EMBEDDING_DIM,
     WGHT_METHOD,
     N_RECOMMENDATIONS,
@@ -129,12 +126,6 @@ for level in metadata_levels.values():
 
         # storing the results
         scores_dict[user] = normalized_user_scores
-
-    # saving scores from cb_descr to parquet
-    if rec_key == RECOMMENDATIONS_KEY_CB_DESCR:
-        print("Saving scores.")
-        scores_df = pd.DataFrame(scores_dict)
-        scores_df.to_parquet(scores_path)
 
     # extract recommendations from scores
     print("Extracting recommendations from scores_dict.")
