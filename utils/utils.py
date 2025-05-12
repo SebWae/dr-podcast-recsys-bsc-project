56dce3e5c4c97605ffd9318a8037bf395668dc48
sewr <sewr@dr.dk>
2025-05-12 21:32:13 +0200

    removed unused functions

--- utils/utils.py
+++ utils/utils.py
@@ -2,20 +2,18 @@ from collections import defaultdict
 import json
 import os
 import sys
-from typing import Iterable, Tuple
+from typing import Iterable
 
 from lenskit.algorithms.als import BiasedMF
 import numpy as np
 import pandas as pd
 from scipy.sparse import csr_matrix
-from scipy.stats import entropy
 from sklearn.metrics.pairwise import cosine_similarity
 
 # adding the parent directory to the Python path
 sys.path.append(os.path.abspath(os.path.join(os.getcwd(), ".")))
 
 from config import (
-    RANDOM_STATE,
     UTILS_PATH,
     UTILS_INTERACTIONS_PATH,
 )
@@ -123,7 +121,7 @@ def format_embedding_dict(emb_dict: dict) -> dict:
     - emb_dict:         Dictionary in the format {"id1": [embedding1], "id2": [embedding2], ...}
 
     Returns:
-    - reshaped_dict:    Dictionary in the format {"episodes": ["id1", "id2", ...], "feature1": [x_11, x_21, ...], "feature2": [x_12, x_22, ...], ...}
+    - formatted_dict:   Dictionary in the format {"episodes": ["id1", "id2", ...], "feature1": [x_11, x_21, ...], "feature2": [x_12, x_22, ...], ...}
     """
     # input dict as dataframe
     emb_df = pd.DataFrame(emb_dict)
@@ -134,7 +132,7 @@ def format_embedding_dict(emb_dict: dict) -> dict:
     # renaming columns
     n_features = len(reshaped_df.columns) - 1
     feature_columns = [f"feature{i+1}" for i in range(n_features)]
-    reshaped_df.columns = ['episode'] + feature_columns
+    reshaped_df.columns = ["episode"] + feature_columns
     formatted_dict = reshaped_df.to_dict(orient="list")
 
     return formatted_dict
@@ -188,13 +186,14 @@ def get_cf_scores(model: BiasedMF,
     Retrieves a dictionary containing scores for relevant items for each user.
 
     Parameters:
-    - model:            Fitted BiasedMF model from lenskit.algorithms.als.  
-    - items:            List of items available for recommendation.
-    - users:            List of users to generate recommendations for. 
-    - item_mapping:     Mapping of item indices to item IDs.
+    - model:                    Fitted BiasedMF model from lenskit.algorithms.als.  
+    - items:                    List of items available for recommendation.
+    - users:                    List of users to generate recommendations for. 
+    - item_mapping:             Mapping of item indices to item IDs.
+    - incl_val_interactions:    Boolean parameter deciding whether to include interactions from validation data or not.
 
     Returns:
-    - scores_dict:      Dictionary of scores for relevant episodes for each user.
+    - scores_dict:              Dictionary of scores for relevant episodes for each user.
     """
     scores_dict = defaultdict(dict)
 
@@ -256,45 +255,6 @@ def get_cf_scores(model: BiasedMF,
             scores_dict[user][prd_to_recommend] = score
 
     return scores_dict
-
-
-def get_hybrid_scores(scores_dict_1: dict,
-                      scores_dict_2: dict,
-                      users: Iterable,
-                      items: Iterable,
-                      _lambda: int) -> dict:
-    """
-    Generates scores for a weighting hybrid recommender. 
-    The scores are computed as a weighted average of the scores from scores_dict_1 and scores_dict_2.
-
-    Parameters:
-    - scores_dict_1:    Dictionary of scores for each item for each user from the first recommender.
-    - scores_dict_2:    Dictionary of scores for each item for each user from the second recommender.
-    - users:            Iterable containing all users.
-    - items:            Iterable containing all items possible to recommend.
-    - _lambda:          Parameter controlling the weighting of scores with weights _lambda and (1 - _lambda).
-
-    Returns:
-    - scores_dict:      Dictionary of scores for each item for each user. 
-    """
-    # initializing scores_dict
-    hybrid_scores = defaultdict(dict)
-
-    # generating hybrid scores for each user
-    for user in users:
-        cf_scores_user = scores_dict_1[user]
-        cb_scores_user = scores_dict_2[user]
-        user_hybrid_scores = {}
-
-        for item in items:
-            cf_score = cf_scores_user[item] if item in cf_scores_user else 0
-            cb_score = cb_scores_user[item] if item in cb_scores_user else 0
-            hybrid_score = _lambda * cf_score + (1 - _lambda) * cb_score
-            user_hybrid_scores[item] = hybrid_score
-
-        hybrid_scores[user] = user_hybrid_scores
-
-    return hybrid_scores
 
 
 def get_pair_weights(n: int) -> dict:
@@ -368,6 +328,7 @@ def get_user_profile(emb_size: int,
     emb_size:       Dimension of embeddings and the user profile. 
     user_int:       Dataframe containing the interactions of the user to build a profile for.
     time_col:       Name of the column containing the temporal data indicating how long time since an interaction took place.
+    item_col:       Name of the column containing item IDs.
     emb_df:         Dictionary containing the embeddings for any item. 
     wght_scheme:    Method to weight the interactions, must be one of 'inverse' or 'linear'.
 
@@ -403,83 +364,6 @@ def get_user_profile(emb_size: int,
     user_profile = user_profile.reshape(1, -1)
 
     return user_profile
-
-
-def permutation_test(dist1: dict, 
-                     dist2: dict, 
-                     N=1000, 
-                     n_permutations=10000) -> Tuple[float, float]:
-    """
-    Computes the KL Divergence between the input distributions, dist1 and dist2.
-    Performs a permutation test to check if dist2 is significantly different from dist1.
-
-    Parameters:
-    - dist1:            Dictionary of category: prob, the target distribution P in the KL formula. 
-    - dist1:            Dictionary of category: prob, the candidate distribution Q in the KL formula.
-    - N:                Size of sample vectors (default value: 100). 
-    - n_permutations:   Number of permutations used in the test (default value: 1000).
-
-    Returns: 
-    - observed_kl:      Observed KL Divergence between dist1 and dist2. 
-    - p_value:          The obtained p-value from the permutation test.
-    """
-    # setting seed
-    np.random.seed(RANDOM_STATE)
-    
-    # obtain all categories
-    dist1_categories = set(dist1.keys())
-    dist2_categories = set(dist2.keys())
-    all_categories = list(dist1_categories.union(dist2_categories))
-
-    # vectors for all categories
-    vec1 = np.array([dist1.get(cat, 0.0) for cat in all_categories])
-    vec2 = np.array([dist2.get(cat, 0.0) for cat in all_categories])
-
-    # Laplace smoothing to avoid division by zero
-    epsilon = 1e-10
-    vec1 += epsilon
-    vec2 += epsilon
-
-    # normalizing probability vectors to make sure they sum to 1
-    vec1 /= vec1.sum()
-    vec2 /= vec2.sum()
-
-    # computing observed KL divergence
-    observed_kl = entropy(vec1, vec2)
-
-    # generating synthetic samples
-    sample1 = np.random.choice(all_categories, size=N, p=vec1)
-    sample2 = np.random.choice(all_categories, size=N, p=vec2)
-
-    # combining samples and initializing sample labels (0 or 1)
-    combined = np.concatenate([sample1, sample2])
-    labels = np.array([0]*N + [1]*N)
-
-    # performing the permutation test
-    permuted_kls = []
-
-    for _ in range(n_permutations):
-        # shuffling the labels
-        np.random.shuffle(labels)
-        g1 = combined[labels == 0]
-        g2 = combined[labels == 1]
-        
-        # generating probability vectors
-        p1 = np.array([np.sum(g1 == cat) for cat in all_categories], dtype=float)
-        p2 = np.array([np.sum(g2 == cat) for cat in all_categories], dtype=float)
-
-        # applying Laplace smoothing 
-        p1 += epsilon
-        p2 += epsilon
-        
-        # computing KL divergence for the current permutation
-        kl = entropy(p1, p2)
-        permuted_kls.append(kl)
-
-    # computing p-value
-    p_value = np.mean(np.array(permuted_kls) >= observed_kl)
-
-    return observed_kl, p_value
 
 
 def prep_interaction_matrix(df: pd.DataFrame, 
