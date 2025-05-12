9b7ae372178be60465c552ebcb83a7206c272ca3
sewr <sewr@dr.dk>
2025-05-12 21:39:47 +0200

    added print statements

--- scripts/04_train_test.py
+++ scripts/04_train_test.py
@@ -19,9 +19,11 @@ from config import (
 
 
 # loading the transformed data
+print("Loading the transformed data.")
 transformed_df = pd.read_parquet(TRANSFORMED_DATA_PATH)
 
 # applying the global user split
+print("Applying global user split.")
 int_train_df = transformed_df[transformed_df["date"] < SPLIT_DATE_TRAIN_VAL]
 int_val_df = transformed_df[(transformed_df["date"] >= SPLIT_DATE_TRAIN_VAL) & (transformed_df["date"] < SPLIT_DATE_VAL_TEST)]
 int_test_df = transformed_df[transformed_df["date"] >= SPLIT_DATE_VAL_TEST]
@@ -41,6 +43,7 @@ train_df["days_since_test"] = (reference_date_test - train_df["date"]).dt.days
 val_df["days_since_test"] = (reference_date_test - val_df["date"]).dt.days
 
 # saving train and test data to parquet files
+print("Saving the train, validation, and test data to the data folder.")
 train_df.to_parquet(TRAIN_DATA_PATH)
 val_df.to_parquet(VAL_DATA_PATH)
 test_df.to_parquet(TEST_DATA_PATH)
